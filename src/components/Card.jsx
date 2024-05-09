b0db4b3e9d321715e40fddc0f304a38d820ab935
Ioana Cosoreanu Seres <ioana.cosoreanu-seres@ibm.com>
2024-05-09 09:23:59 +0300

    changes to contact, navbar and cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,7 @@ const Card = ({id, icon, title, content}) => {
     <div className='flex justify-start flex-col md:px-10 md:py-5 py-0 px-2 rounded-[20px] max-w-[500px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card'>
       <img src={icon} alt={title} className='w-[52px] object-contain'/>
       <h4 className='font-poppins font-bold text-[20px] leading-[32px] text-white my-5'> {title}</h4>
-      <p className='font-poppins font-normal text-[18px] leading-[32px] text-white my-5'> {content}</p>
+      <p className='font-poppins font-normal text-[18px] leading-[32px] text-dimWhite my-5'> {content}</p>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
     <div className='sm:hidden flex flex-1 justify-end items-center'>
       <img src={toggle ? close: menu} alt="menu" className='w-[28px] h-[28px] object-contain' 
       onClick={()=>setToggle(prev=>!prev)} />
-      <div className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar `}>
+      <div className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[340px] min-h-[200px] rounded-xl sidebar `}>
       <ul className='list-none flex flex-col justify-end items-center flex-1'>
       {navLinks.map((navLink, index)=>(
         <li key={navLink.id} className={`font-poppins font-normal cursor-pointer text-[16px] ${index ===navLinks.length-1 ? 'mr-0' : 'mb-4' } text-white `}>
